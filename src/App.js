ddc27b4f18f3e8b850caab9f78902ad5bad1bb82
Lakisha J <lakishajohnson@pursuit.org>
2021-11-12 10:10:42 -0500

    A few methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
+import Heading from './components/Heading';
 import './App.css';
+import CalculatorKeys from './components/CalculatorKeys';
+import Results from './components/Results';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+      history: "",
+    }
+  }
+
+  buttonClicked = button => {
+    if(button === "=") {
+      this.calculate()
+    } else if (button === "CE") {
+      this.backspace()
+    } else if (button === "C") {
+      this.reset()
+    } else {
+      this.setState({
+        result: this.state.result + button
+      })
+    } 
+  }
+  // calculate = () => {}
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  }
+  
+  reset = () => {
+    this.setState({
+      result: "",
+    })
+  }
+    
+
+  render() {
+    return (
+        <div className="App">
+      <Heading />
+      <Results result={this.state.result}/>
+      <CalculatorKeys buttonClicked={this.onClick}/>
     </div>
-  );
+   ); 
+  }
 }
-
+  
 export default App;
+
+  
+
+
+/**function Display(props){
+    return (
+        <div className = 'containerDisplay'>
+            <p className = 'numeros'>{props.numero1} {props.operador} {props.numero2}</p>
+            <p className = 'resultado'>{props.resultado}</p>
+        </div>
+    )
+} */
